6f21d2f68576accbdac05427c46b1d3701b996cf
pokimoki99 <48154115+pokimoki99@users.noreply.github.com>
2019-10-03 16:13:49 +0100

    prot

---
+++ Gun Gale Online/Assets/Player.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Photon.MonoBehaviour
+{
+    public float speed = 10f;
+    private float lastSynchronizationTime = 0f;
+    private float syncDelay = 0f;
+    private float syncTime = 0f;
+   // private Vector3 syncStartPosition=Vector3.zero
+
+    void Update()
+    {
+        if (photonView.isMine)
+        {
+            InputMovement();
+            InputColorChange();
+        }
+    }
+
+    void InputMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position
+                + Vector3.forward * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.S))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position
+                - Vector3.forward * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.D))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position
+                + Vector3.right * speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.A))
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position
+                - Vector3.right * speed * Time.deltaTime);
+
+    }
+
+    private void InputColorChange()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+            ChangeColorTo(new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f),
+                Random.Range(0f, 1f)));
+    }
+
+    [PunRPC] void ChangeColorTo (Vector3 color)
+    {
+        GetComponent<Renderer>().material.color = new Color(color.x, color.y, color.z, 1f);
+        if (photonView.isMine)
+            photonView.RPC("ChangeColorTo", PhotonTargets.OthersBuffered, color);
+    }
+}
