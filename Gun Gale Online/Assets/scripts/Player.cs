276dc9ba9e9b89ff3ac62af06ee2087290eb517e
pokimoki99 <48154115+pokimoki99@users.noreply.github.com>
2019-10-17 18:01:04 +0100

    test

--- Gun Gale Online/Assets/scripts/Player.cs
+++ Gun Gale Online/Assets/scripts/Player.cs
@@ -15,18 +15,22 @@ public class Player : Photon.MonoBehaviour
     {
         if (stream.isWriting)
         {
-            stream.SendNext(GetComponent<Rigidbody>().position);
-            stream.SendNext(GetComponent<Rigidbody>().velocity);
+            //stream.SendNext(GetComponent<Rigidbody>().position);
+            //stream.SendNext(GetComponent<Rigidbody>().velocity);
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
         }
         else
         {
-            Vector3 syncPosition = (Vector3)stream.ReceiveNext();
-            Vector3 syncVelocity = (Vector3)stream.ReceiveNext();
-            syncTime = 0f;
-            syncDelay = Time.time - lastSynchronizationTime;
-            lastSynchronizationTime = Time.time;
-            syncEndPosition = syncPosition + syncVelocity * syncDelay;
-            syncStartPosition = GetComponent<Rigidbody>().position;
+            //Vector3 syncPosition = (Vector3)stream.ReceiveNext();
+            //Vector3 syncVelocity = (Vector3)stream.ReceiveNext();
+            Vector3 syncTransformPos = (Vector3)stream.ReceiveNext();
+            Quaternion rot = (Quaternion)stream.ReceiveNext();
+            //syncTime = 0f;
+            //syncDelay = Time.time - lastSynchronizationTime;
+            //lastSynchronizationTime = Time.time;
+            //syncEndPosition = syncTransformPos * syncDelay;
+            //syncStartPosition = GetComponent<Transform>().position;
         }
     }
 
@@ -43,34 +47,46 @@ public class Player : Photon.MonoBehaviour
         }
         else
         {
-            SynchedMovement();
+            //SynchedMovement();
         }
     }
 
     void InputMovement()
     {
+        //if (Input.GetKey(KeyCode.W))
+        //    GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position
+        //        + Vector3.forward * speed * Time.deltaTime);
+
+        //if (Input.GetKey(KeyCode.S))
+        //    GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position
+        //        - Vector3.forward * speed * Time.deltaTime);
+
+        //if (Input.GetKey(KeyCode.D))
+        //    GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position
+        //        + Vector3.right * speed * Time.deltaTime);
+
+        //if (Input.GetKey(KeyCode.A))
+        //    GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position
+        //        - Vector3.right * speed * Time.deltaTime);
+
         if (Input.GetKey(KeyCode.W))
-            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position
-                + Vector3.forward * speed * Time.deltaTime);
+            GetComponent<Transform>().Translate(Vector3.forward * speed * Time.deltaTime);
 
         if (Input.GetKey(KeyCode.S))
-            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position
-                - Vector3.forward * speed * Time.deltaTime);
+            GetComponent<Transform>().Translate(Vector3.back * speed * Time.deltaTime);
 
         if (Input.GetKey(KeyCode.D))
-            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position
-                + Vector3.right * speed * Time.deltaTime);
+            GetComponent<Transform>().Translate(Vector3.right * speed * Time.deltaTime);
 
         if (Input.GetKey(KeyCode.A))
-            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position
-                - Vector3.right * speed * Time.deltaTime);
+            GetComponent<Transform>().Translate(Vector3.left * speed * Time.deltaTime);
 
     }
 
     private void SynchedMovement()
     {
         syncTime += Time.deltaTime;
-        GetComponent<Rigidbody>().position = Vector3.Lerp(syncStartPosition,
+        GetComponent<Transform>().position = Vector3.Lerp(syncStartPosition,
             syncEndPosition, syncTime / syncDelay);
     }
 
