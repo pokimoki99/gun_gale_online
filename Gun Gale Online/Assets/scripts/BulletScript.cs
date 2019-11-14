b38391108bcfd125af2adfed0f26a375de57e798
pokimoki99 <48154115+pokimoki99@users.noreply.github.com>
2019-11-14 17:29:23 +0000

    bullets work

--- Gun Gale Online/Assets/scripts/BulletScript.cs
+++ Gun Gale Online/Assets/scripts/BulletScript.cs
@@ -6,48 +6,74 @@ public class BulletScript : MonoBehaviour
 
     GameManager gm;
     GameObject explosion;
+    public GameObject bullet;
     public GameObject explosionprefab;
     public float force = 500.0f;
+    public bool shotgun_spread=false;
+    Vector3 rand;
 
     // Use this for initialization
     void Start()
     {
-        GetComponent<Rigidbody>().AddForce(transform.forward * force);
+        if (shotgun_spread == true)
+        {
+            //GetComponent<Rigidbody>().AddForce((transform.forward+rand) * force);
+            GetComponent<Rigidbody>().AddForce(rand * force);
+            //bullet.transform.Rotate(0.0f,bullet.transform.rotation.y + 10.0f,0.0f);
+            Debug.Log("work?");
+
+        }
+        else
+        {
+            GetComponent<Rigidbody>().AddForce(transform.forward * force);
+        }
+
         gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+
     }
 
     void OnCollisionEnter(Collision col)
     {
-        if (col.gameObject.tag == "EnemyNPC")
-        {
-            gm.incscore(1);
-            Destroy(col.gameObject);// the cube
-            explosion = Instantiate(explosionprefab, this.transform.position, this.transform.rotation) as GameObject;
-            Destroy(explosion, 5.0f); // the explosion
-        }
+        //if (col.gameObject.tag == "EnemyNPC")
+        //{
+        //    gm.incscore(1);
+        //    Destroy(col.gameObject);// the cube
+        //    explosion = Instantiate(explosionprefab, this.transform.position, this.transform.rotation) as GameObject;
+        //    Destroy(explosion, 5.0f); // the explosion
+        //}
         if (col.gameObject.tag == "Terrain")
+        {
+            Destroy(gameObject);
+
+        }
+        if (col.gameObject.tag == "Player")
         {
             gm.incscore(10);
             Destroy(gameObject);
 
         }
-        else if (col.gameObject.tag == "Sphere")
-        {
-            gm.incscore(20);
-            Destroy(col.gameObject);// the cube
-            explosion = Instantiate(explosionprefab, this.transform.position, this.transform.rotation) as GameObject;
-            Destroy(explosion, 5.0f); // the explosion
-        }
+        //else if (col.gameObject.tag == "Sphere")
+        //{
+        //    gm.incscore(20);
+        //    Destroy(col.gameObject);// the cube
+        //    explosion = Instantiate(explosionprefab, this.transform.position, this.transform.rotation) as GameObject;
+        //    Destroy(explosion, 5.0f); // the explosion
+        //}
 
-        Destroy(gameObject); // the bullet
+
+        //Destroy(gameObject); // the bullet
 
     }
     void main()
     {
-        if (gameObject.transform.position.y <=0)
+        if (gameObject.transform.position.y <= 0)
         {
             Destroy(gameObject);
         }
-        
+
+    }
+    public void random()
+    {
+        rand = new Vector3(0, Random.Range(0.0f, 1.0f), 0);
     }
 }
---
+++ Gun Gale Online/Assets/scripts/shotgunfirescript.cs
@@ -0,0 +1,53 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class shotgunfirescript : MonoBehaviour
+{
+
+    public Transform bulletprefab;
+    public GameObject sGun_pos;
+    Vector3 gun;
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Input.GetButtonDown("Fire1") && (GameManager.Instance.ammocount > 0))
+        {
+            gun = new Vector3(sGun_pos.transform.position.x + 0.5f, sGun_pos.transform.position.y + 0.4f, sGun_pos.transform.position.z + 0.5f);
+            Instantiate(bulletprefab, sGun_pos.transform.position, transform.rotation);
+            GameManager.Instance.fire();
+            gun = new Vector3(sGun_pos.transform.position.x + 0.45f, sGun_pos.transform.position.y + 0.4f, sGun_pos.transform.position.z + 0.5f);
+            Instantiate(bulletprefab, sGun_pos.transform.position, transform.rotation);
+            GameManager.Instance.fire();
+            gun = new Vector3(sGun_pos.transform.position.x + 0.55f, sGun_pos.transform.position.y + 0.4f, sGun_pos.transform.position.z + 0.5f);
+            Instantiate(bulletprefab, sGun_pos.transform.position, transform.rotation);
+            GameManager.Instance.fire();
+            gun = new Vector3(sGun_pos.transform.position.x + 0.5f, sGun_pos.transform.position.y + 0.45f, sGun_pos.transform.position.z + 0.5f);
+            Instantiate(bulletprefab, sGun_pos.transform.position, transform.rotation);
+            GameManager.Instance.fire();
+            gun = new Vector3(sGun_pos.transform.position.x + 0.5f, sGun_pos.transform.position.y + 0.35f, sGun_pos.transform.position.z + 0.5f);
+            Instantiate(bulletprefab, sGun_pos.transform.position, transform.rotation);
+            GameManager.Instance.fire();
+            gun = new Vector3(sGun_pos.transform.position.x + 0.55f, sGun_pos.transform.position.y + 0.45f, sGun_pos.transform.position.z + 0.5f);
+            Instantiate(bulletprefab, sGun_pos.transform.position, transform.rotation);
+            GameManager.Instance.fire();
+            gun = new Vector3(sGun_pos.transform.position.x + 0.45f, sGun_pos.transform.position.y + 0.45f, sGun_pos.transform.position.z + 0.5f);
+            Instantiate(bulletprefab, sGun_pos.transform.position, transform.rotation);
+            GameManager.Instance.fire();
+            gun = new Vector3(sGun_pos.transform.position.x + 0.55f, sGun_pos.transform.position.y + 0.35f, sGun_pos.transform.position.z + 0.5f);
+            Instantiate(bulletprefab, sGun_pos.transform.position, transform.rotation);
+            GameManager.Instance.fire();
+            gun = new Vector3(sGun_pos.transform.position.x + 0.45f, sGun_pos.transform.position.y + 0.35f, sGun_pos.transform.position.z + 0.5f);
+            Instantiate(bulletprefab, sGun_pos.transform.position, transform.rotation);
+            GameManager.Instance.fire();
+
+        }
+    }
+}
