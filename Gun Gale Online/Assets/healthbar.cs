b94931da609f500541a1a3135d8e297a4acb0047
ShadowKnight20 <48155299+ShadowKnight20@users.noreply.github.com>
2019-11-24 15:28:13 +0000

    Healthbar V1.1

---
+++ Gun Gale Online/Assets/healthbar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class healthbar : MonoBehaviour
+{
+    private Image barImage;
+    private Health health;
+
+    private void Awake()
+    {
+        barImage = transform.Find("Bar").GetComponent<Image>();
+
+        health = new Health();
+    }
+    private void Update()
+    {
+        health.Update();
+
+        barImage.fillAmount = health.GetHealthNormalized();
+    }
+}
+public class Health
+{
+    public const int HEALTH_MAX = 100;
+
+    private float healthAmount;
+    private float healthRegenAmount;
+
+    public Health()
+    {
+        healthAmount = 0;
+        healthRegenAmount = 10f;
+    }
+    public void Update()
+    {
+        healthAmount += healthRegenAmount * Time.deltaTime;
+    }
+    public void Damage(int DamageAmount)
+    {
+        if (healthAmount >= DamageAmount)
+        {
+            healthAmount -= DamageAmount;
+        }
+    }
+    public float GetHealthNormalized()
+    {
+        return healthAmount / HEALTH_MAX;
+    }
+}
